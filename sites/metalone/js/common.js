bb95fb6f200823ca026b5784facd18449e460292
RovanLab <rovanlab@gmail.com>
2017-09-26 20:01:07 +0530

    jvone & metalone

---
+++ sites/metalone/js/common.js
@@ -0,0 +1,128 @@
+
+
+$(document).ready(function(){
+	
+	
+	jQuery("#layerslider").layerSlider({
+			pauseOnHover: false,
+			responsive: true,
+			responsiveUnder: 1400,
+			layersContainer: 1400,
+			skinsPath: 'css/skins/'
+		});
+		
+	
+
+$('.owl-carousel2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:4
+        },
+		320:{
+			items:1
+		},
+		360:{
+			items:1
+		},
+        600:{
+            items:2
+        },
+		640:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+
+$('.owl-carousel3').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    responsive:{
+        0:{
+            items:4
+        },
+		320:{
+			items:1
+		},
+		360:{
+			items:1
+		},
+        600:{
+            items:2
+        },
+		640:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+
+$('.owl-carousel4').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:4
+        },
+		320:{
+			items:1
+		},
+		360:{
+			items:1
+		},
+        600:{
+            items:3
+        },
+		640:{
+            items:2
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+$('.owl-carousel5').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+		320:{
+			items:1
+		},
+		360:{
+			items:1
+		},
+        600:{
+            items:1
+        },
+		640:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+	
+		
+		
+		
+		
+		
+		
+});
+
+
