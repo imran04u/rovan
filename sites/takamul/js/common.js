36a1595cd1422fd75fa12ad1a4e33b57ca93bf32
RovanLab <rovanlab@gmail.com>
2018-04-01 17:37:40 +0530

    update

---
+++ sites/takamul/js/common.js
@@ -0,0 +1,37 @@
+ // Header Fixed   
+ $(window).scroll(function () {
+  var height = $(window).scrollTop();
+  if (height >3) {
+    $('header').addClass('header-fixed');
+  }
+  else if (height == 0) {
+    $('header').removeClass('header-fixed');
+  }
+});  
+
+
+
+$(document).ready(function() {
+ 
+if($(window).width() <= 1024){
+	 $("#menu").mmenu({
+	   "offCanvas": {
+		 "position": "right"
+	   }
+	 });  
+	}
+
+
+
+ 
+	
+});
+
+
+
+
+
+
+
+
+
